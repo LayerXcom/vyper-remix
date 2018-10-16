279d76ca937469cfc02cb5d2cabfa5cf6bc867d9
nrryuya <nrryuya@gmail.com>
2018-10-16 21:34:41 +0900

    remove "[Recommended] "

--- app/src/App.js
+++ app/src/App.js
@@ -188,7 +188,7 @@ class App extends Component {
         <div style={{ background: "white", margin: "1em 2em", padding: "1.5em 0" }}>
           <Radio type="radio" name="compile" value="host" onChange={() => this.setState({ compileDst: "host" })} checked={this.state.compileDst === 'host'} label="Host" />
           <Popup trigger={<Icon name="question circle" />}
-            content="[Recommended] You can compile the vyper code using remote server."
+            content="You can compile the vyper code using remote server."
             basic
           />
           <Radio type="radio" name="compile" value="local" onChange={() => this.setState({ compileDst: "local" })} checked={this.state.compileDst === 'local'} label="Local" style={{ marginLeft: "1em" }} />
